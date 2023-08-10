e1b1033dbec47aebb4cf2c7f68e5916f4832e09d
Nazar <you@example.com>
2023-08-10 16:56:08 +0200

    created view for showing post page and modified some templates inheritances

--- src/post_app/views.py
+++ src/post_app/views.py
@@ -4,7 +4,7 @@ from django.views.generic.edit import View
 
 from .forms import CreatePostForm
 from .models import Post
-from common.services import create_object, get_queryset
+from common.services import create_object, get_queryset, get_object_data, check_is_anonymous_user, check_object_is_none
 
 
 class CreatePostView(View):
@@ -12,7 +12,7 @@ class CreatePostView(View):
 
     def get(self, request):
         form = CreatePostForm()
-        return render(request, self.template_name, {"form": form})
+        return render(request, template_name=self.template_name, context={"form": form})
 
     def post(self, request):
         form = CreatePostForm(request.POST, request.FILES)
@@ -25,10 +25,23 @@ class CreatePostView(View):
             return redirect("../create_post/")
 
 
-class MyPostsView(View):
+class MyPostsListView(View):
     model = Post
     template_name = "post_app/my_posts.html"
 
     def get(self, request):
+        if check_is_anonymous_user(request.user):
+            return redirect("../../common/page_404")
         posts = get_queryset(self.model, user=request.user)
-        return render(request, self.template_name, {"posts": posts})
+        return render(request, template_name=self.template_name, context={"posts": posts})
+
+
+class PostView(View):
+    template_name = "post_app/post.html"
+    model = Post
+
+    def get(self, request, pk: int):
+        post = get_object_data(model=self.model, pk=pk)
+        if check_object_is_none(obj=post):
+            return redirect("../../../common/page_404")
+        return render(request, template_name=self.template_name, context={"post": post})
