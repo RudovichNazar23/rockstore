5a1e7813a86ea6673db1fee156c99d2bc957a0f8
RudovichNazar23 <n.rudovich2005@gmail.com>
2023-09-15 20:26:51 +0200

    commited docker-compose.yml properly

--- src/src/settings.py
+++ src/src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k059+nhuc4cdr3mo0qs@_6q5^@_4tpb^z$h^i$p$jngtpa^$9e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 SITE_ID = 3
@@ -105,26 +105,26 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rockstore_project',
+        'USER': 'rockstore_user',
+        'PASSWORD': 'rockstore_project_31',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'rockstore_project',
-#         'USER': 'rockstore_user',
-#         'PASSWORD': 'rockstore_project_31',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
 #         'HOST': 'db',
 #         'PORT': '5432',
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
